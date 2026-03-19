adbf9fffd365589422a7492fd3885ab20d2a92bc
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-03-19 10:59:26 +0600

    Create controller class and write all CRUD HTTP method endpoint.

---
+++ student-management/src/main/java/com/anik/student_management/controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.anik.student_management.controller;
+
+import com.anik.student_management.model.Student;
+import com.anik.student_management.service.IStudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final IStudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents(){
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        return new ResponseEntity<>(studentService.addStudent(student), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student){
+        return new ResponseEntity<>(studentService.updateStudent(id, student), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id){
+        return new ResponseEntity<>(studentService.getStudentById(id), HttpStatus.OK);
+    }
+}
